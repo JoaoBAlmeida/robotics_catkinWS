6cd047cb9bf1b6c73f30d3cc36062b54d787f74b
JoaoBAlmeida <jpbarbosinha@gmail.com>
2020-09-18 19:30:56 -0300

    Workplace Catkin with Learning Joystick package

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/barbosa/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/barbosa/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/barbosa/catkin_ws/devel;/opt/ros/melodic' != '' else []
